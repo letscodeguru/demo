d3dacd653d3c0c2e24a661626c638a4a1cc55537
kunal Chaudhari <kunalpict@gmail.com>
2018-04-10 21:23:11 -0400

    updated for war deployer

--- src/main/java/com/mywork/service/demo/DemoApplication.java
+++ src/main/java/com/mywork/service/demo/DemoApplication.java
@@ -1,12 +1,87 @@
 package com.mywork.service.demo;
 
+
+import java.time.LocalDate;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableSwagger2
+public class DemoApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+
+	  @Bean
+	  public Docket petApi() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	        .select()
+	          .apis(RequestHandlerSelectors.any())
+	          .paths(PathSelectors.any())
+	          .build()
+	        .pathMapping("/")
+	        .directModelSubstitute(LocalDate.class,
+	            String.class)
+	        .genericModelSubstitutes(ResponseEntity.class)
+	        .useDefaultResponseMessages(false);
+	  }
+
+	  @Bean
+	  SecurityConfiguration security() {
+	    return SecurityConfigurationBuilder.builder()
+	        .clientId("test-app-client-id")
+	        .clientSecret("test-app-client-secret")
+	        .realm("test-app-realm")
+	        .appName("test-app")
+	        .scopeSeparator(",")
+	        .additionalQueryStringParams(null)
+	        .useBasicAuthenticationWithAccessCodeGrant(false)
+	        .build();
+	  }
+
+	  @Bean
+	  UiConfiguration uiConfig() {
+	    return UiConfigurationBuilder.builder()
+	        .deepLinking(true)
+	        .displayOperationId(false)
+	        .defaultModelsExpandDepth(1)
+	        .defaultModelExpandDepth(1)
+	        .defaultModelRendering(ModelRendering.EXAMPLE)
+	        .displayRequestDuration(false)
+	        .docExpansion(DocExpansion.NONE)
+	        .filter(false)
+	        .maxDisplayedTags(null)
+	        .operationsSorter(OperationsSorter.ALPHA)
+	        .showExtensions(false)
+	        .tagsSorter(TagsSorter.ALPHA)
+	        .validatorUrl(null)
+	        .build();
+	  }
 }
+
