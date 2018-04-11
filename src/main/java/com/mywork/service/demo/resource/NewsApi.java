e85aab266d8a8b49d4f75c15db936a43eff3b832
kunal Chaudhari <kunalpict@gmail.com>
2018-04-10 22:39:35 -0400

    added resource path

--- src/main/java/com/mywork/service/demo/resource/NewsApi.java
+++ src/main/java/com/mywork/service/demo/resource/NewsApi.java
@@ -1,6 +1,7 @@
 package com.mywork.service.demo.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,9 @@ import com.mywork.service.demo.services.NewsFetchService;
 
 @RestController
 @RequestMapping(path = "/news")
+@ComponentScan(basePackageClasses = {
+	NewsApi.class
+})
 public class NewsApi {
 
 	@Autowired
