f1d9446c1f11242ec1ed80ff3b9d27aab7695932
kunal Chaudhari <kunalpict@gmail.com>
2018-04-10 23:43:04 -0400

    updating swagger basepath

--- src/main/java/com/mywork/service/demo/DemoApplication.java
+++ src/main/java/com/mywork/service/demo/DemoApplication.java
@@ -45,7 +45,7 @@ public class DemoApplication  extends SpringBootServletInitializer {
 	          .apis(RequestHandlerSelectors.any())
 	          .paths(PathSelectors.any())
 	          .build()
-	        .pathMapping("/")
+	        .pathMapping("/api/")
 	        .directModelSubstitute(LocalDate.class,
 	            String.class)
 	        .genericModelSubstitutes(ResponseEntity.class)
