d084493c7928a32b7e8b44d0c7a817112122b26b
kunal Chaudhari <kunalpict@gmail.com>
2018-04-11 00:28:04 -0400

    webhook

--- src/main/java/com/mywork/service/demo/services/impl/NewsFetchServiceImpl.java
+++ src/main/java/com/mywork/service/demo/services/impl/NewsFetchServiceImpl.java
@@ -9,7 +9,7 @@ public class NewsFetchServiceImpl implements NewsFetchService {
 
 	@Override
 	public String latestNews() {
-		return "This is latest news. with new news. and jenkins.cdddd";
+		return "This is latest news. with new news. and jenkins.aadded webhook";
 	}
 
 }
